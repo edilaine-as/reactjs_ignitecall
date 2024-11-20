c99f12f8d369d56011ffa76ee3adf2c0ea41c930
Edilaine Alves dos Santos <91744159+edilaine-as@users.noreply.github.com>
2024-11-20 14:35:24 -0300

    feat: add schedule username

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,63 @@
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { prisma } from '../../../lib/prisma'
+import { Container, UserHeader } from './styles'
+
+interface ScheduleProps {
+  user: {
+    name: string
+    bio: string
+    avatarUrl: string
+  }
+}
+
+export default function Schedule({ user }: ScheduleProps) {
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={user.avatarUrl} />
+        <Heading>{user.name}</Heading>
+        <Text>{user.bio}</Text>
+      </UserHeader>
+    </Container>
+  )
+}
+
+// npm build gera páginas estaticas 
+// quais usernames eu quero querar páginas estaticas desde a primeira build
+// no caso abaixo não vai gerar nenhuma página estatica no momento da build, só vai gerar quando a página for acessada
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+    revalidate: 60 * 60 * 24, // 1 day
+    // indica de quanto em quanto tempo eu quero que essa pagina seja recriada
+  }
+
+}
